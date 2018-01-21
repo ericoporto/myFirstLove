2bbb02ff7bf709215eb17bcf06f3c009b22c0378
ericoporto <ericoporto2008@gmail.com>
2018-01-21 21:38:41 -0200

    shaders AND text

--- project/main.lua
+++ project/main.lua
@@ -155,22 +155,15 @@ function love.load(arg)
 
 	--shader test from rxi
 	shader_screen = love.graphics.newShader[[
-    vec2 radialDistortion(vec2 coord, float dist) {
-      vec2 cc = coord - 0.5;
-      dist = dot(cc, cc) * dist + cos(1.5 * .3) * .01;
-      return (coord + cc * (1.0 + dist) * dist);
-    }
+		extern vec2 abberationVector;
 
-
-    vec4 effect(vec4 color, Image tex, vec2 tc, vec2 pc) {
-      vec2 tcr = radialDistortion(tc, .24)  + vec2(.001, 0);
-      vec2 tcg = radialDistortion(tc, .20);
-      vec2 tcb = radialDistortion(tc, .18) - vec2(.001, 0);
-      vec4 res = vec4(Texel(tex, tcr).r, Texel(tex, tcg).g, Texel(tex, tcb).b, 1)
-        - cos(tcg.y * 128. * 3.142 * 2) * .03
-        - sin(tcg.x * 128. * 3.142 * 2) * .03;
-      return res * Texel(tex, tcg).a;
-    }
+		vec4 effect(vec4 color, Image currentTexture, vec2 texCoords, vec2 screenCoords){
+			vec4 finalColor = vec4(1);
+			finalColor.r = Texel(currentTexture, texCoords.xy + abberationVector).r;
+			finalColor.g = Texel(currentTexture, texCoords.xy).g;
+			finalColor.b = Texel(currentTexture, texCoords.xy - abberationVector).b;
+			return finalColor;
+		}
   ]]
 end
 
--- project/src/states/Game.lua
+++ project/src/states/Game.lua
@@ -8,6 +8,8 @@ local Vector        = requireLibrary("hump.vector")
 local tween         = timer.tween
 local a
 local b
+local strength
+local cnv
 
 Game = Gamestate.new()
 
@@ -26,12 +28,28 @@ end
 local function drawFn()
     -- <Your drawing logic goes here.>
     -- love.graphics.draw(padLeft,a,2)
-    love.graphics.setFont(font_Verdana2)
-    love.graphics.print(love.timer.getFPS(), 1, 1)
-    love.graphics.rectangle("fill", 5+10*b, 50*b, 60, 120 )
-  end
+    love.graphics.setShader()
+    cnv = love.graphics.newCanvas(320,180)
+    cnv:renderTo(function()
+        love.graphics.setColor( 255, 255, 255, 255 )
+        love.graphics.setFont(font_Verdana2)
+        love.graphics.print("teste de texto\n a\nb", 80+20*b, 25)
+        love.graphics.rectangle("fill", 30+12*b, 30+15*b, 16, 32 )
+    end)
+
+
+    love.graphics.setShader(shader_screen)
+    strength = math.sin(love.timer.getTime()*2)
+    shader_screen:send("abberationVector", {strength*math.sin(love.timer.getTime()*7)/200, strength*math.cos(love.timer.getTime()*7)/200})
+
+    love.graphics.draw(cnv,0,0)
+    
+end
 
 function Game:draw()
-    love.graphics.setShader(shader_screen)
+
+
     screen:draw(drawFn) -- Additional arguments will be passed to drawFn.
+
+
 end
