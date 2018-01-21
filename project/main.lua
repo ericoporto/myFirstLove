ff7a66d71c8915da3c649b7a26e4842bfdfd2ea0
ericoporto <ericoporto2008@gmail.com>
2018-01-21 18:30:32 -0200

    shader test

--- project/main.lua
+++ project/main.lua
@@ -34,6 +34,9 @@ font_SullyVerge = nil
 font_TimesNewPixel = nil
 font_Verdana2 = nil
 
+-- shader screen
+shader_screen = nil
+
 -- Global Functions inspired by picolove https://github.com/gamax92/picolove/blob/master/api.lua
 function all(a)
 	if a==nil or #a==0 then
@@ -149,6 +152,26 @@ function love.load(arg)
 	font_SullyVerge = love.graphics.newFont("fonts/SullyVerge.ttf", 16)
 	font_TimesNewPixel = love.graphics.newFont("fonts/TimesNewPixel.ttf", 16)
 	font_Verdana2 = love.graphics.newFont("fonts/Verdana2.ttf", 16)
+
+	--shader test from rxi
+	shader_screen = love.graphics.newShader[[
+    vec2 radialDistortion(vec2 coord, float dist) {
+      vec2 cc = coord - 0.5;
+      dist = dot(cc, cc) * dist + cos(1.5 * .3) * .01;
+      return (coord + cc * (1.0 + dist) * dist);
+    }
+
+
+    vec4 effect(vec4 color, Image tex, vec2 tc, vec2 pc) {
+      vec2 tcr = radialDistortion(tc, .24)  + vec2(.001, 0);
+      vec2 tcg = radialDistortion(tc, .20);
+      vec2 tcb = radialDistortion(tc, .18) - vec2(.001, 0);
+      vec4 res = vec4(Texel(tex, tcr).r, Texel(tex, tcg).g, Texel(tex, tcb).b, 1)
+        - cos(tcg.y * 128. * 3.142 * 2) * .03
+        - sin(tcg.x * 128. * 3.142 * 2) * .03;
+      return res * Texel(tex, tcg).a;
+    }
+  ]]
 end
 
 -- Logic
--- project/src/states/Game.lua
+++ project/src/states/Game.lua
@@ -6,17 +6,20 @@ local Gamestate     = requireLibrary("hump.gamestate")
 local timer         = requireLibrary("hump.timer")
 local Vector        = requireLibrary("hump.vector")
 local tween         = timer.tween
+local a
+local b
 
 Game = Gamestate.new()
 
 local stuff = {}
 function Game:enter()
-
-	
+    a=0
+	b=0
 end
 
 function Game:update(dt)
-   
+   a=a+1
+   b=math.cos(a/32)
 end
 
 
@@ -25,8 +28,10 @@ local function drawFn()
     -- love.graphics.draw(padLeft,a,2)
     love.graphics.setFont(font_Verdana2)
     love.graphics.print(love.timer.getFPS(), 1, 1)
+    love.graphics.rectangle("fill", 5+10*b, 50*b, 60, 120 )
   end
 
 function Game:draw()
+    love.graphics.setShader(shader_screen)
     screen:draw(drawFn) -- Additional arguments will be passed to drawFn.
 end
