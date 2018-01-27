25a000476ac6f5b4c825c8ed1cfbbd334efef4c1
ericoporto <ericoporto2008@gmail.com>
2018-01-27 01:20:45 -0200

    adding a working camera

--- project/src/states/Game.lua
+++ project/src/states/Game.lua
@@ -5,28 +5,68 @@
 local Gamestate     = requireLibrary("hump.gamestate")
 local timer         = requireLibrary("hump.timer")
 local Vector        = requireLibrary("hump.vector")
+local Camera        = requireLibrary("hump.camera")
 local tween         = timer.tween
 local map
 local a
 local b
 local strength
 local cnv
+local player
+local camera
 
 Game = Gamestate.new()
 
 local stuff = {}
 function Game:enter()
+  player = {
+    pos = {
+      x = 64,
+      y = 128
+    }
+  }
+
+  camera = Camera(player.pos.x, player.pos.y)
   map = sti("map/test.lua")
   a=0
   b=0
 end
 
 function Game:update(dt)
+
+  
   if keys_pressed['up'] then
-    a=a+1
-    b=math.cos(a/32)
-    map:update(dt)
+    if player.pos.y>0 then
+      player.pos.y=player.pos.y-4
+    end
   end
+
+  if keys_pressed['down'] then
+    if player.pos.y<map.height*map.tileheight then
+      player.pos.y=player.pos.y+4
+    end
+  end
+
+  if keys_pressed['left'] then
+    if player.pos.x>0 then
+      player.pos.x=player.pos.x-4
+    end
+  end
+
+  if keys_pressed['right'] then
+    if player.pos.x<map.width*map.tilewidth then
+      player.pos.x=player.pos.x+4
+    end
+  end
+
+  a=a+1
+  b=math.cos(a/32)
+
+  local dx = player.pos.x - camera.x
+  local dy = player.pos.y - camera.y
+  map:update(dt)
+  camera:move(dx/2, dy/2)
+
 end
 
 
@@ -37,9 +77,36 @@ local function drawFn()
     cnv = love.graphics.newCanvas(320,180)
     cnv:renderTo(function()
 
-        -- mapa
-        map:draw()
 
+        local tx = camera.x - love.graphics.getWidth() / 2
+        local ty = camera.y - love.graphics.getHeight() / 2
+    
+        if tx < 0 then 
+            tx = 0 
+        end
+        if ty < 0 then 
+            ty = 0 
+        end
+        --  if tx > map.width  * map.tilewidth  - love.graphics.getWidth()  then
+        --      tx = map.width  * map.tilewidth  - love.graphics.getWidth()  
+        --  end
+        if ty > map.height * map.tileheight - love.graphics.getHeight() then
+            ty = map.height * map.tileheight - love.graphics.getHeight()
+        end
+    
+        tx = math.floor(tx)
+        ty = math.floor(ty)
+
+        print("tx = " , tostring(tx) , "; ty = " , tostring(ty))
+
+
+	    map:draw(-tx, -ty, camera.scale, camera.scale)
+
+
+        camera:draw(function()
+            
+        end)
+        -- mapa
 
         -- zuera
         love.graphics.setColor( 255, 255, 255, 255 )
